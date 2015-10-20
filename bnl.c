3b3ee286fc76339f9581b18581046be2b36cb6d1
Frédéric <frederic.lamorce@gmail.com>
2015-10-20 19:56:05 -0400

    fix indent

--- bnl.c
+++ bnl.c
@@ -29,19 +29,19 @@
 #include <zlib.h>
 
 static char debug=0;
-#define Dlog(M, ...)	do { if(debug) {fprintf(stderr, M "\n", ##__VA_ARGS__); } } while(0)
+#define Dlog(M, ...)  do { if(debug) {fprintf(stderr, M "\n", ##__VA_ARGS__); } } while(0)
 
 // return filesize in bytes
 long long fsize(FILE *f)
 {
-		long long filesize;
-		off_t cur_offset = ftello(f);		// save current offset
-		
-		fseeko(f, 0, SEEK_END);   			// may be non-portable
+    long long filesize;
+    off_t cur_offset = ftello(f);   // save current offset
+
+    fseeko(f, 0, SEEK_END);         // may be non-portable
     filesize=ftello(f);
-    
-    fseeko(f, cur_offset, SEEK_SET);	// reset to previous offset
-    
+
+    fseeko(f, cur_offset, SEEK_SET);  // reset to previous offset
+
     return filesize;
 }
 
@@ -49,180 +49,180 @@ long long fsize(FILE *f)
 // rounded up
 unsigned int nb_blocks(long long filesize, unsigned long blocksize)
 {
-	unsigned int nb;
-	
-	nb=(long long)(filesize / blocksize);
-	if((long long)(filesize % blocksize))
-		nb++;
+  unsigned int nb;
 
-	return nb;
+  nb=(long long)(filesize / blocksize);
+  if((long long)(filesize % blocksize))
+    nb++;
+
+  return nb;
 }
 
 // create a CRC file for a given file and block size
 int create_crc_file(char *filename, char *filename_crc, unsigned long bs)
 {
-	FILE *f;
-	FILE *fcrc;						// CRC destination file 
-	unsigned int i, nb;
-	long long filesize;
-	char *buf;						// buffer to read block
-	size_t bufread;
-	unsigned long cur_crc=crc32(0L, Z_NULL, 0);	// current block CRC
+  FILE *f;
+  FILE *fcrc;           // CRC destination file
+  unsigned int i, nb;
+  long long filesize;
+  char *buf;            // buffer to read block
+  size_t bufread;
+  unsigned long cur_crc=crc32(0L, Z_NULL, 0); // current block CRC
 
-	Dlog("C opening file %s", filename);
-	f=fopen(filename, "rb");
-	if(f==NULL)
-	{
-			fprintf(stderr, "Cannot open file %s\n", filename);
-			return -1;
-	}
-	
-	// open CRC destination file
-	fcrc=fopen(filename_crc, "wb");
-	if(fcrc==NULL)
-	{
-		fprintf(stderr, "Cannot create CRC dest file %s\n", filename_crc);
-		return -2;
-	}
-	
-	// get file size
+  Dlog("C opening file %s", filename);
+  f=fopen(filename, "rb");
+  if(f==NULL)
+  {
+      fprintf(stderr, "Cannot open file %s\n", filename);
+      return -1;
+  }
+
+  // open CRC destination file
+  fcrc=fopen(filename_crc, "wb");
+  if(fcrc==NULL)
+  {
+    fprintf(stderr, "Cannot create CRC dest file %s\n", filename_crc);
+    return -2;
+  }
+
+  // get file size
   filesize=fsize(f);
   Dlog("C size %llu bytes", filesize);
 
-	// calculate number of block required
-	nb=nb_blocks(filesize, bs);
-	Dlog("C number of blocks required : %u", nb);
+  // calculate number of block required
+  nb=nb_blocks(filesize, bs);
+  Dlog("C number of blocks required : %u", nb);
 
-	fwrite(&bs, 1, sizeof(unsigned long), fcrc);
+  fwrite(&bs, 1, sizeof(unsigned long), fcrc);
 
-	buf=(char*)malloc(bs);
-	// loop to read the nb blocks
-	for(i=0; i<nb; i++)
-	{
-			bufread=fread(buf, 1, bs, f);
-			// calculate CRC for block
-			cur_crc=crc32(0, buf, bufread);
-			Dlog("C block %05u : %08lX", i, cur_crc);
-			fwrite(&cur_crc, 1, sizeof(unsigned long), fcrc);
-	}
+  buf=(char*)malloc(bs);
+  // loop to read the nb blocks
+  for(i=0; i<nb; i++)
+  {
+      bufread=fread(buf, 1, bs, f);
+      // calculate CRC for block
+      cur_crc=crc32(0, buf, bufread);
+      Dlog("C block %05u : %08lX", i, cur_crc);
+      fwrite(&cur_crc, 1, sizeof(unsigned long), fcrc);
+  }
 
-	// cleanup
-	free(buf);
+  // cleanup
+  free(buf);
 #ifdef __linux__
-	syncfs(fileno(fcrc));
+  syncfs(fileno(fcrc));
 #endif
-	fclose(fcrc);
-	fclose(f);
+  fclose(fcrc);
+  fclose(f);
 
-	return 0;
+  return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	int opt;
-	char *source, *dest, *dest_crc;
-	FILE *fs, *fd;				// source, destination
-	FILE *fcrc=NULL;			// CRC destination file 
-	unsigned int bsMB=8;	// block size in MB, default 8MB
-	unsigned long bs=bsMB<<20;			// block size in bytes, default 8MB
-	unsigned int i, nb;
-	long long filesize;
-	char *buf;						// buffer to read block
-	size_t bufread;
-	unsigned long cur_crc=crc32(0L, Z_NULL, 0);	// current block CRC
-	unsigned long *crc_block;		// array CRC for each blocks
-	char changed=0;
-	char create_dest=0;
-	
-	while ((opt = getopt(argc, argv, "b:dh")) != -1)
-	{
-		switch (opt)
-		{
-			case 'd':
-				debug=1;
-				break;
-			case 'b':
-				bsMB=strtoul(optarg, NULL, 0);
-				bs=bsMB<<20;
-				Dlog("setting block size to %u MB from arg", bsMB);
-				if(bsMB!=0)
-					break;
-			case 'h':
-			default: /* '?' */
-				fprintf(stderr, "Usage: %s [-h] [-d] [-b n] <source> <destination>\n", argv[0]);
-				fprintf(stderr, "       -h   : this help screen\n");
-				fprintf(stderr, "       -d   : enable debug log\n");
-				fprintf(stderr, "       -b n : set block size to n MB for creation (default 8)\n");
-				exit(-1);
-		}
-	}
+  int opt;
+  char *source, *dest, *dest_crc;
+  FILE *fs, *fd;        // source, destination
+  FILE *fcrc=NULL;      // CRC destination file
+  unsigned int bsMB=8;  // block size in MB, default 8MB
+  unsigned long bs=bsMB<<20;      // block size in bytes, default 8MB
+  unsigned int i, nb;
+  long long filesize;
+  char *buf;            // buffer to read block
+  size_t bufread;
+  unsigned long cur_crc=crc32(0L, Z_NULL, 0); // current block CRC
+  unsigned long *crc_block;   // array CRC for each blocks
+  char changed=0;
+  char create_dest=0;
 
-	source = argv[optind];
-	dest = argv[optind+1];
+  while ((opt = getopt(argc, argv, "b:dh")) != -1)
+  {
+    switch (opt)
+    {
+      case 'd':
+        debug=1;
+        break;
+      case 'b':
+        bsMB=strtoul(optarg, NULL, 0);
+        bs=bsMB<<20;
+        Dlog("setting block size to %u MB from arg", bsMB);
+        if(bsMB!=0)
+          break;
+      case 'h':
+      default: /* '?' */
+        fprintf(stderr, "Usage: %s [-h] [-d] [-b n] <source> <destination>\n", argv[0]);
+        fprintf(stderr, "       -h   : this help screen\n");
+        fprintf(stderr, "       -d   : enable debug log\n");
+        fprintf(stderr, "       -b n : set block size to n MB for creation (default 8)\n");
+        exit(-1);
+    }
+  }
 
-	if(optind >= argc || source==NULL || dest==NULL)
-	{
+  source = argv[optind];
+  dest = argv[optind+1];
+
+  if(optind >= argc || source==NULL || dest==NULL)
+  {
         fprintf(stderr, "Expected source and destination file names\n");
         exit(-1);
   }
 
   // name for the CRC file
-	dest_crc=(char*)malloc(strlen(dest)+5);
-	sprintf(dest_crc, "%s.crc", dest);
+  dest_crc=(char*)malloc(strlen(dest)+5);
+  sprintf(dest_crc, "%s.crc", dest);
 
-	// we need the source file, can't do anything without it
-	Dlog("opening source file %s", source);
-	fs=fopen(source, "rb");
-	if(fs==NULL)
-	{
-			fprintf(stderr, "Cannot open source file %s\n", source);
-			exit(-2);
-	}
+  // we need the source file, can't do anything without it
+  Dlog("opening source file %s", source);
+  fs=fopen(source, "rb");
+  if(fs==NULL)
+  {
+      fprintf(stderr, "Cannot open source file %s\n", source);
+      exit(-2);
+  }
 
-	// open destination file
-	Dlog("opening destination file %s", dest);
-	fd=fopen(dest, "r+b");
-	if(fd==NULL)
-	{
-			// it's ok if the file does not exist, we create it
-			fprintf(stderr, "Destination file %s does not exist, create it\n", dest);
-			create_dest=1;	// remember we are creating it, mostly for display purpose
-			fd=fopen(dest, "wb");
-			if(fd==NULL)
-			{
-				// if we really cannot create the file, no other choice than exit
-				fprintf(stderr, "Cannot create dest file %s\n", dest);
-				exit(-3);
-			}
-	}
+  // open destination file
+  Dlog("opening destination file %s", dest);
+  fd=fopen(dest, "r+b");
+  if(fd==NULL)
+  {
+      // it's ok if the file does not exist, we create it
+      fprintf(stderr, "Destination file %s does not exist, create it\n", dest);
+      create_dest=1;  // remember we are creating it, mostly for display purpose
+      fd=fopen(dest, "wb");
+      if(fd==NULL)
+      {
+        // if we really cannot create the file, no other choice than exit
+        fprintf(stderr, "Cannot create dest file %s\n", dest);
+        exit(-3);
+      }
+  }
   else
   {
     // Destination file exists! open its associated CRC destination file
-	  Dlog("opening destination CRC file %s", dest_crc);
-	  fcrc=fopen(dest_crc, "rb");
-	  if(fcrc==NULL)
-	  {
-		  // CRC file does not exist, but destination file exists
+    Dlog("opening destination CRC file %s", dest_crc);
+    fcrc=fopen(dest_crc, "rb");
+    if(fcrc==NULL)
+    {
+      // CRC file does not exist, but destination file exists
       // so let's create CRC file based on its content,
       // read >> write so if it's already a backup, it will be
       // faster to write only changed blocks.
       Dlog("creating CRC file %s for %s", dest_crc, dest);
-		  if(create_crc_file(dest, dest_crc, bs)<0)
-		  {
-				fprintf(stderr, "Failed to create CRC file %s for %s", dest_crc, dest);
-				exit(-4);
-			}
-		  // file now exists, re-open it
-		  fcrc=fopen(dest_crc, "rb");
-		  if(fcrc==NULL)	// you kidding me?
-		  {
-				fprintf(stderr, "Error opening CRC file %s\n", dest_crc);
-				exit(-5);
-			}
-	  }
+      if(create_crc_file(dest, dest_crc, bs)<0)
+      {
+        fprintf(stderr, "Failed to create CRC file %s for %s", dest_crc, dest);
+        exit(-4);
+      }
+      // file now exists, re-open it
+      fcrc=fopen(dest_crc, "rb");
+      if(fcrc==NULL)  // you kidding me?
+      {
+        fprintf(stderr, "Error opening CRC file %s\n", dest_crc);
+        exit(-5);
+      }
+    }
 
-	  // read blocksize from file
-	  bufread=fread(&bs, 1, sizeof(unsigned long), fcrc);
+    // read blocksize from file
+    bufread=fread(&bs, 1, sizeof(unsigned long), fcrc);
     if(ferror(fcrc))
     {
       fprintf(stderr, "Error reading block size from CRC file %s\n", dest_crc);
@@ -230,97 +230,97 @@ int main(int argc, char *argv[])
     }
   }
 
-	bsMB=bs>>20;
-	Dlog("setting blocksize at %u MB", bsMB);
-	
-	// get source file size
+  bsMB=bs>>20;
+  Dlog("setting blocksize at %u MB", bsMB);
+
+  // get source file size
   filesize=fsize(fs);
   Dlog("source size %llu bytes", filesize);
 
-	// calculate number of block required
-	nb=nb_blocks(filesize, bs);
-	Dlog("number of blocks required : %u", nb);
+  // calculate number of block required
+  nb=nb_blocks(filesize, bs);
+  Dlog("number of blocks required : %u", nb);
 
-	// allocate memory for the number of block
-	crc_block=(unsigned long*)calloc(nb, sizeof(unsigned long));
-	// read array of dest CRC in memory, if CRC file is opened and valid
+  // allocate memory for the number of block
+  crc_block=(unsigned long*)calloc(nb, sizeof(unsigned long));
+  // read array of dest CRC in memory, if CRC file is opened and valid
   if(fcrc)
   {
-  	bufread=fread(crc_block, 1, nb*sizeof(unsigned long), fcrc);
-  	fclose(fcrc);
+    bufread=fread(crc_block, 1, nb*sizeof(unsigned long), fcrc);
+    fclose(fcrc);
   }
 
-	// allocate for the buffer size
-	buf=(char*)malloc(bs);
-	// loop to read the nb blocks
-	for(i=0; i<nb; i++)
-	{
-			bufread=fread(buf, 1, bs, fs);
-			if(ferror(fs))
-			{
-				fprintf(stderr, "Error %d reading source file %s\n", errno, source);
-				exit(-10);
-			}
-			// calculate CRC for source block
-			cur_crc=crc32(0, buf, bufread);
-			if(create_dest)
-				Dlog("block %05u : source %08lX", i, cur_crc);
-			else
-				Dlog("block %05u : source %08lX, dest %08lX", i, cur_crc, crc_block[i]);
-			// test if current CRC differs from destination CRC block
-			// or force write if we have to create destination
-			if(create_dest || cur_crc != crc_block[i])
-			{
-				if(create_dest)
-					printf("Creating block %05u\n", i);
-				else
-					printf("Block %05u differ from %08lX, copying block\n", i, crc_block[i]);
-				// seek at right block position to write block
-				fseeko(fd, bs*i, SEEK_SET);
-				fwrite(buf, 1, bufread, fd);
-				if(ferror(fd))
-				{
-					fprintf(stderr, "Error %d writing destination file %s\n", errno, dest);
-					exit(-11);
-				}
-				// update CRC
-				crc_block[i]=cur_crc;
-				changed=1;
-			}
-			printf("%u%%\r", (i*100)/nb);
-			fflush(stdout);
-	}
-	printf("100%%\n");
-	
-	// flush new CRC array if needed
-	if(changed)
-	{
-		Dlog("creating destination CRC file %s", dest_crc);
-		fcrc=fopen(dest_crc, "wb");
-		if(fcrc==NULL)
-		{
-			fprintf(stderr, "Error creating CRC file %s\n", dest_crc);
-			exit(-12);
-		}
-		fwrite(&bs, 1, sizeof(unsigned long), fcrc);
-		fwrite(crc_block, 1, nb*sizeof(unsigned long), fcrc);
-#ifdef __linux__
-		syncfs(fileno(fcrc));
-#endif
-		fclose(fcrc);
-#ifdef __linux__
-		// as something changed in destination, flush the file too
-		syncfs(fileno(fd));
-#endif
-	}
-	puts("done.");
+  // allocate for the buffer size
+  buf=(char*)malloc(bs);
+  // loop to read the nb blocks
+  for(i=0; i<nb; i++)
+  {
+      bufread=fread(buf, 1, bs, fs);
+      if(ferror(fs))
+      {
+        fprintf(stderr, "Error %d reading source file %s\n", errno, source);
+        exit(-10);
+      }
+      // calculate CRC for source block
+      cur_crc=crc32(0, buf, bufread);
+      if(create_dest)
+        Dlog("block %05u : source %08lX", i, cur_crc);
+      else
+        Dlog("block %05u : source %08lX, dest %08lX", i, cur_crc, crc_block[i]);
+      // test if current CRC differs from destination CRC block
+      // or force write if we have to create destination
+      if(create_dest || cur_crc != crc_block[i])
+      {
+        if(create_dest)
+          printf("Creating block %05u\n", i);
+        else
+          printf("Block %05u differ from %08lX, copying block\n", i, crc_block[i]);
+        // seek at right block position to write block
+        fseeko(fd, bs*i, SEEK_SET);
+        fwrite(buf, 1, bufread, fd);
+        if(ferror(fd))
+        {
+          fprintf(stderr, "Error %d writing destination file %s\n", errno, dest);
+          exit(-11);
+        }
+        // update CRC
+        crc_block[i]=cur_crc;
+        changed=1;
+      }
+      printf("%u%%\r", (i*100)/nb);
+      fflush(stdout);
+  }
+  printf("100%%\n");
 
-	// cleanup
-	free(dest_crc);
-	free(buf);
-	free(crc_block);
-	fclose(fd);
-	fclose(fs);
+  // flush new CRC array if needed
+  if(changed)
+  {
+    Dlog("creating destination CRC file %s", dest_crc);
+    fcrc=fopen(dest_crc, "wb");
+    if(fcrc==NULL)
+    {
+      fprintf(stderr, "Error creating CRC file %s\n", dest_crc);
+      exit(-12);
+    }
+    fwrite(&bs, 1, sizeof(unsigned long), fcrc);
+    fwrite(crc_block, 1, nb*sizeof(unsigned long), fcrc);
+#ifdef __linux__
+    syncfs(fileno(fcrc));
+#endif
+    fclose(fcrc);
+#ifdef __linux__
+    // as something changed in destination, flush the file too
+    syncfs(fileno(fd));
+#endif
+  }
+  puts("done.");
 
-	return 0;
+  // cleanup
+  free(dest_crc);
+  free(buf);
+  free(crc_block);
+  fclose(fd);
+  fclose(fs);
+
+  return 0;
 }
